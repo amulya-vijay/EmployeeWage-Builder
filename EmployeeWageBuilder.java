cf8124e7644c92f7ab97ae155f676991c84a83ad
amulya-vijay <amulyakvijay297@gmail.com>
2021-06-07 11:56:38 +0530

    'employeeAttendance' method is implemented to check if the employee is present.

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,10 +1,23 @@
 package employeeWageBuilder;
 
 public class EmployeeWageBuilder {
+	
+	public static String employeeAttendance() {
+		
+		int value = (int) Math.floor(Math.random()*2+1);
+		boolean isEmployeePresent = (value==1) ? true  : false ;
+		if(isEmployeePresent) {
+			return "Employee is Present";
+		}else {
+			return "Employee is Absent";
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program.");
+		System.out.println(employeeAttendance());
 		
 	}
 }
